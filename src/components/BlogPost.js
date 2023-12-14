1ed6960a89a4be67f7280c6ed15001c03b79abd9
AlexChenl3 <alex.chen13@icloud.com>
2023-12-14 10:25:42 -0500

    blog

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -5,9 +5,11 @@ import styles from '../css/BlogPost.module.css';
 function BlogPost({ title, content, num_likes }) {
   return (
     <div className={styles["blog-post"]}>
-      <h2>{title}</h2>
-      <p className={styles["blog-text"]}>{content}</p>
-      <p className={styles["blog-text"]}>{num_likes}</p>
+      <div className={styles['blog-inner']}>
+        <h2>{title}</h2>
+        <p className={styles["blog-text"]}>{content}</p>
+        <p className={styles["blog-text"]}>{num_likes}</p>
+      </div>
     </div>
   );
 }
